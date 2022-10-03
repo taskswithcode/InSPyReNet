4713a96daf1d84a0a2c05ffceafc0c04c1de3746
taskswithcode <farminaround@gmail.com>
2022-10-03 06:39:32 +0200

    Fixes

--- twc_setup.sh
+++ twc_setup.sh
@@ -1,4 +1,5 @@
-#apt-get update
-#apt-get install ffmpeg libsm6 libxext6  -y
+set -x
+apt-get update
+apt-get install ffmpeg libsm6 libxext6  -y
 mkdir -p data/backbone_ckpt
 cp twc_models/latest.pth data/backbone_ckpt/swin_base_patch4_window12_384_22kto1k.pth
